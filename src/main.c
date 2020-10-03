86567b6c59e47cf7ba134994222c17ad29c58a5c
Stepan <st085873@gmail.com>
2020-10-03 21:17:51 +0300

    modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -5,7 +5,6 @@
 #define MAX_RESULT_STRING_LENGTH 80
 #define array_size(x) (sizeof(x) / sizeof(x[0]))
 #define error(...) (fprintf(stderr, __VA_ARGS__))
-#define PI 3.14
 
 struct interval_t {
     double left_border;
@@ -27,7 +26,7 @@ int read_interval(struct interval_t *interval) {
         error("Cannot read from stdin");
         return -1;
     }
-    if(interval->right_border > PI) {
+    if(interval->right_border > M_PI) {
         error("Right border must be <= PI");
         return -1;
     }
@@ -42,23 +41,25 @@ int read_interval(struct interval_t *interval) {
     return 0;
 }
 
-double square_function(struct interval_t interval) {
-    return sin((interval.right_border + interval.left_border) / 2) * (interval.right_border - interval.left_border);
+double integrate_by_square_function(struct interval_t interval) {
+    double lb = interval.left_border, rb = interval.right_border;
+    return sin((rb + lb) / 2) * (rb - lb);
 }
 
-double sympson_function(struct interval_t interval) {
-    return (interval.right_border - interval.left_border) * (sin(interval.left_border) + 4 * sin((interval.left_border + interval.right_border) / 2) + sin(interval.right_border)) / 6;
+double integrate_by_sympson_function(struct interval_t interval) {
+    double lb = interval.left_border, rb = interval.right_border;
+    return (rb - lb) * (sin(lb) + 4 * sin((lb + rb) / 2) + sin(rb)) / 6;
 }
 
 double integrate(struct interval_t interval, unsigned int accuracy, double (*integral_function)(struct interval_t)) {
-    double S = 0;
+    double area = 0;
     double step = (interval.right_border - interval.left_border) / (double)accuracy;
     for(unsigned int i = 0; i < accuracy; i++) {
         interval.right_border = interval.left_border + step;
-        S += integral_function(interval);
+        area += integral_function(interval);
         interval.left_border = interval.right_border;
     }
-    return S;
+    return area;
 }
 
 char** calculate_integrals(struct interval_t interval, const unsigned int accuracies[], size_t experiments_count) {
@@ -79,9 +80,9 @@ char** calculate_integrals(struct interval_t interval, const unsigned int accura
     //Интегрирование и заполнение массива строк
     for(int i = 0; i < experiments_count; i++) {
         int accuracy = accuracies[i];
-        double S1 = integrate(interval, accuracy, square_function);
-        double S2 = integrate(interval, accuracy, sympson_function);
-        if(sprintf(results[i], "%d %.5lf %.5lf", accuracy, S1, S2) == 0) {
+        double area1 = integrate(interval, accuracy, integrate_by_square_function);
+        double area2 = integrate(interval, accuracy, integrate_by_sympson_function);
+        if(sprintf(results[i], "%d %.5lf %.5lf", accuracy, area1, area2) == 0) {
             free_array_of_string(results, experiments_count);
             error("Cannot write result %d in results", i);
             return NULL;
@@ -99,7 +100,7 @@ void free_array_of_string(char** array, size_t size) {
 
 int main() {
     struct interval_t interval = {0.0, 0.0};
-    unsigned int accuracies[] = {5, 10, 20, 100, 500, 1000};
+    unsigned int accuracies[] = {6, 10, 20, 100, 500, 1000};
     size_t experiments_count = array_size(accuracies);
     //считывание значений границ
     if(read_interval(&interval) < 0)
